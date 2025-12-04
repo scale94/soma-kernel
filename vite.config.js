8a3275017997405449f2ed4ed0d11a79bb997919
scale <admin@soma.kernel>
2025-12-04 23:27:14 +0100

    Final config: Removed base path for Cloudflare Pages root domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/soma-kernel/", // <-- This fixes the blank page error on GitHub Pages
 })
